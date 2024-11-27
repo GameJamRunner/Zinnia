913f2e95e52601cd13d0e94ce550288c2151a4ea
cle <cleanmartini.draw@gmail.com>
2024-11-27 00:42:44 -0800

    add more portraits

--- game/code/Desktop/desktop.gd
+++ game/code/Desktop/desktop.gd
@@ -5,4 +5,4 @@ const Balloon = preload("res://game/code/DialogueWidgets/desktop_balloon.tscn")
 func _ready():
 	var balloon: Node = Balloon.instantiate()
 	get_tree().current_scene.add_child(balloon)
-	balloon.start(load("res://game/dialogues/day_01.dialogue"), "day_1")
+	balloon.start(load("res://game/dialogues/day_01.dialogue"), "day_01")
