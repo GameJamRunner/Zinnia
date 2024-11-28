8d83df5ab901f1a26a9dd7f0dbc379b77f9bb81e
cle <cleanmartini.draw@gmail.com>
2024-11-27 16:28:21 -0800

    transition scene

--- game/code/Desktop/desktop.gd
+++ game/code/Desktop/desktop.gd
@@ -1,31 +1,48 @@
 extends Node2D
 
 const Balloon = preload("res://game/code/DialogueWidgets/desktop_balloon.tscn")
+const DIALOGUES = {
+	1: "res://game/dialogues/day_01.dialogue",
+	2: "res://game/dialogues/day_02.dialogue",
+	3: "res://game/dialogues/day_03.dialogue"
+}
 
 var balloon: Node
 
 func _ready():
+	_setup_balloon()
+	if PlayerStats.current_day in DIALOGUES:
+		_start_dialogue(PlayerStats.current_day)
+
+func _setup_balloon():
 	balloon = Balloon.instantiate()
 	get_tree().current_scene.add_child(balloon)
 	balloon.day_completed.connect(on_day_completed)
-	if PlayerStats.current_day == 1:
-		balloon.start(load("res://game/dialogues/day_01.dialogue"), "day_01")
+
+func _start_dialogue(day: int):
+	var dialogue_path = DIALOGUES.get(day)
+	if dialogue_path:
+		balloon.start(load(dialogue_path), "day_%02d" % day)
+	else:
+		print("Dialogue not found for day: " + str(day))
 
 func on_day_completed():
+	await _play_animation("fade_to_transition")
+	_update_day_ui()
+
+func _update_day_ui():
 	%DayLabel.text = "Day " + str(PlayerStats.current_day)
-	%AnimationPlayer.play("fade_to_transition")
-	await %AnimationPlayer.animation_finished
-	%AnimationPlayer.play("fade_out_transition")
-	await %AnimationPlayer.animation_finished
-	
-	if PlayerStats.current_day == 2:
-		balloon = Balloon.instantiate()
-		get_tree().current_scene.add_child(balloon)
-		balloon.start(load("res://game/dialogues/day_02.dialogue"), "day_02")
-	elif PlayerStats.current_day == 3:
-		balloon = Balloon.instantiate()
-		get_tree().current_scene.add_child(balloon)
-		balloon.start(load("res://game/dialogues/day_03.dialogue"), "day_03")
+	%NextDayButton.text = "Day %d\n-> Continue" % (PlayerStats.current_day)
+
+func _on_next_day_button_pressed():
+	if PlayerStats.current_day in DIALOGUES:
+		_setup_balloon()
+		_start_dialogue(PlayerStats.current_day)
 	else:
 		print("Invalid current day: " + str(PlayerStats.current_day))
-		
+		return
+	await _play_animation("fade_out_transition")
+
+func _play_animation(animation_name: String) -> void:
+	%AnimationPlayer.play(animation_name)
+	await %AnimationPlayer.animation_finished
