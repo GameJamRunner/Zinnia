bd95840823d7bacdeca10a4de331310196a18eea
cle <cleanmartini.draw@gmail.com>
2024-11-30 13:52:39 -0800

    title cg

---
+++ game/code/TitleScene/title_scene.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+func _on_button_pressed():
+	$BlackPanel
+	$AudioStreamPlayer.play()
+	$AnimationPlayer.play("start_game")
+	await $AnimationPlayer.animation_finished
+	get_tree().change_scene_to_file("res://game/code/Desktop/desktop.tscn")
